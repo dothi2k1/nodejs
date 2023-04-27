f6654936d54df1a2e2f8fd06ed9f92297ec8b5ee
DoThi <thidovan58@gmail.com>
2023-04-27 11:27:40 +0700

    them sql nodejs

--- webnodejsne/src/app/controllers/NewController.js
+++
@@ -1,9 +0,0 @@
-class NewController {
-    index(red, res) {
-        res.render('news');
-    }
-    show(red, res) {
-        res.render('news/tin1');
-    }
-}
-module.exports = new NewController;
--- webnodejsne/src/app/controllers/SiteController.js
+++
@@ -1,9 +0,0 @@
-class SiteController {
-    index(red, res) {
-        res.render('home');
-    }
-    intro(red, res) {
-        res.render('intro');
-    }
-}
-module.exports = new SiteController;
